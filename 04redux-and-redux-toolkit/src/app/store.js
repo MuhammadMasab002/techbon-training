ebfb05950b314ae5cef3cae293d6dd90a94449a4
Muhammad Masab <muhammadmasab2002@gmail.com>
2025-02-03 14:33:09 +0500

    create todo and counter

---
+++ 04redux-and-redux-toolkit/src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import todoReducer from "../features/todo/todoSlice";
+import countReducer from "../features/counter/countSlice";
+
+export const store = configureStore({
+  // reducer: todoReducer
+  reducer: {
+    todo: todoReducer,
+    count: countReducer,
+    
+  },
+  //devTools: true, // Enable Redux DevTools in development mode
+});
---
+++ 04redux-and-redux-toolkit/src/components/counter/count-value.jsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+
+const CountValue = () => {
+  const state = useSelector((state) => state.count.count);
+  // const { count } = state;
+  return (
+    <div>
+      <p className="text-2xl text-white font-bold">{state}</p>
+    </div>
+  );
+};
+
+
+export default CountValue;
